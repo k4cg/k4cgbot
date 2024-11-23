9af3c07b23116d7b81a13bdf38b700ac4cdf274c
max <max@karl.wf>
2024-11-23 15:37:32 +0100

    Bump version to 1.0.0

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let pkgs = import <nixpkgs> { };
 in pkgs.buildGoModule rec {
   pname = "k4cgbot";
-  version = "0.0.1";
+  version = "1.0.0";
   src = pkgs.lib.cleanSource ./.;
-  vendorHash = "sha256-rhizQadk+DfsD/iqW61/xzTQSZn5VnMERtkgHtCeJns=";
+  vendorHash = "sha256-i+Z43KrxYAJ39L88yTpH7KX3/y+r7o/R54evOoDYTiI=";
 }
