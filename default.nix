4e313542af043dff613509a1a6f750cfad0a3586
Max <max@karl.wf>
2025-01-01 18:57:12 +0100

    Bump version to 1.1.0

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let pkgs = import <nixpkgs> { };
 in pkgs.buildGoModule rec {
   pname = "k4cgbot";
-  version = "1.0.0";
+  version = "1.1.0";
   src = pkgs.lib.cleanSource ./.;
   vendorHash = "sha256-i+Z43KrxYAJ39L88yTpH7KX3/y+r7o/R54evOoDYTiI=";
 }
