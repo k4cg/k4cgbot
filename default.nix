5771f255c21cba2ec20099dcab80db2c2df12834
Max <max@karl.wf>
2025-04-17 18:47:58 +0200

    Update nix hash

--- default.nix
+++ default.nix
@@ -3,5 +3,5 @@ in pkgs.buildGoModule rec {
   pname = "k4cgbot";
   version = "1.1.0";
   src = pkgs.lib.cleanSource ./.;
-  vendorHash = "sha256-i+Z43KrxYAJ39L88yTpH7KX3/y+r7o/R54evOoDYTiI=";
+  vendorHash = "sha256-g90As9KIgCqlMB+8OByEeDMF2YwZy5Mz4vqKbdPuRXo=";
 }
